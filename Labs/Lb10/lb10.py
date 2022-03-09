4c76d7117e23dd96cbe93c62978165db25c268bf
Mrs. Ramirez Luna <Mrs.ramirezq502@gmail.com>
2022-03-09 19:43:50 +0000

    mode

---
+++ Labs/Lb10/lb10.py
@@ -0,0 +1,235 @@
+#Instructor example
+fname="Luna" #this creates a variable for my name
+print(fname)
+bday=1955 # this is a integer variable
+bplace="Caracas" #this is a string variable
+print (bday , bplace)
+answer=input("What's your favorite color? ")
+print("Hola! my name is "+fname+ " I like the color "+answer+ " too :)")
+print ("I was born in "+bplace, bday)
+#group practice write your code under this line
+
+#Rockeya
+spyYoda="I watched all the jurassic park movies, I'm a nerd"
+print(spyYoda)
+#Rockeyaone
+nerdSpider="I hate spiders!"
+print(nerdSpider)
+year=2022 # this is the new year and example of integer variable
+feelings="happy"# how i feel, also example of a string variable    
+print (year , feelings)
+print ("I am feeling"+" "+feelings)
+reply=input("How are you feeling?")
+print("You are feeling"+" "+reply)
+
+#Rui
+favoriteColor="Pink" #Creating a variable for favorite color
+print(favoriteColor)
+nextTravelPlace="Greece"#Creating a variable next travel place
+print(nextTravelPlace)
+storeNumber=12345 #this is a integer variable
+storeName="CVS" #this is a string variable
+print(storeNumber, storeName)
+print("I like to go to " +  storeName, storeNumber)
+answer=input("what is your favortite snack from CVS store?")
+print("Interesting,that you like "+answer+". I actaully like lays chips") 
+      
+#my name is vincent variable
+itisnotme="Vincent" 
+print(itisnotme)
+month=1 		#integer variable
+day="today"		#string variable
+print(month , day)
+print("what is "+ day+" "+"is the" ,month, day)
+asw=input("what is today's day?")
+print(asw +" is a good day.")
+
+
+#Jaspal  
+firstname="This is Jaspal"
+print(firstname)
+numberofclasses=8 #This is  an example of an integer variable
+nameofschool="Queensborough" #This is an example of a string variable
+print (numberofclasses , nameofschool)
+print("I took" ,numberofclasses, nameofschool)
+answer=input ("what month are we in?")
+print("The answer "+answer+" is correct")
+
+
+
+
+#shemane
+beautiful="This is Shemane" 
+print("beautiful")
+month= "December" #This an integer varible
+fromplace="Brooklyn" #This is string variable
+print (month, fromplace)
+print ("I am from " + fromplace+" " + month )
+answers=input ("what's the date your born in December?")
+print("I was born on the 25" +fname+" I like the month" +answers+" too")
+      
+#Caroline
+fname="I am Caroline"
+lname="Russell"
+print(fname,lname)
+ccard_Number="2223434444" # This is an integer
+cname="MasterCard" # This is an string variable
+print(ccard_Number, cname)
+print("I love to shop with my " + cname , ccard_Number)
+answer=input("Do you have a credit card?")
+print("Great!" "Enter card number" +answer+ "Thank you!")
+
+#Andre
+fname1="This is Andre"
+print(fname1)
+bday=2021 #this is a interger variable
+bplace="Antigua" #this is a string variable
+print(bday , bplace)
+print("A little about me"+" " +bplace, bday)
+answer=input("What's your favorite Borough? ")
+print("Welcome! my name is "+fname1+ " I like  "+answer+ " too")
+print ("I was born in "+bplace, bday)
+
+#Nataliya
+fn="Nataliya" #a variable for my name#
+print(fn)
+areacode=347 #an integer
+city="NYC"  #string variable
+print(city, areacode)
+print("City I live in is " + city)      
+print("the area code is ", areacode)
+answer=input("What city do you live? ")
+print("Hello, my name is "+fn+". I live in "+answer+" too...:)")
+
+#Chanie 
+fname123="This is Chanda"
+print(fname123)
+birthmonth=11 #this is an integer variable
+mname="Nicole" #this is a string variable
+print(birthmonth, mname)
+reply=input("What's your favorite zodiac sign?")
+print("Eurika!" " " +fname123+ "the"+reply+"is known to be passionate and charming")
+print("Birthday Month", birthmonth)
+print("In the workforce I use my middle name "+mname) 
+
+      
+      
+      
+#qiuyi
+myfirstname="Qiuyi" 
+print(myfirstname)
+newyear=2022 # this is a integer variable
+goal="having new year resolution" #this is a string variable
+print (newyear, goal)
+      
+print("new year means "+goal,newyear)
+
+reply=input("What do you like to eat")
+print("my name is "+myfirstname+ "I love to eat"+reply+ "too and we can eat together!")
+
+    
+      
+      
+      
+#Ilana
+iname="Ilana" #This creates a variable for my name
+print(iname)
+please=1991 #This is a interger variable
+thankyou="Queens" #This is a string variable
+print(please , thankyou)
+print("I was born in " + thankyou , please)
+answer=input("What's your favorite food? ")
+print("Hi, my name is" +iname+ "I like to eat " +answer+ " too!")
+
+#Matthew
+dontcopyme="Matthew"
+print(dontcopyme)
+dingus=25 #this is the integer for my age
+favoriteconsole="Nintendo Gamecube" #this is the variable for my favorite console
+print(dingus, favoriteconsole)
+print("My favorite console is the "+favoriteconsole+" and I am",dingus)
+bingo=input("What is your favorite game console?")
+print("Your favorite game console is "+bingo+"? That's lame because I like the "+favoriteconsole+". :P")
+
+#Lisa
+Knownname="Lisa"
+print(Knownname)
+Knownname="Lisa" # This creates a variable for my name
+anniversary= 25 #this is an interger value
+eventlocation= "jamaica" #this is a string variable
+print(anniversary, eventlocation)
+print("I was married", anniversary, eventlocation)
+answer=input("How old are you?")
+print("Hey y'all, my name is" +knownname+ "I am " +answer)  
+            
+#thisisDebo
+fname1="Debo"
+print(fname)
+bday=1200
+flightschool="World"
+print("Hello"+flightschool)
+reply=input("Whatisyourfavoritefood")
+print("Thereplywas"+reply+"is")
+      
+
+      
+
+
+#Manpal
+hello="Manpal"
+print(hello)
+year=1886 #first successful gasoline driven car
+planet="Earth"
+print(year , planet)
+print("The first gas automobile was driven on planet " +planet+ " in the year", year)
+answer=input("What year were you born?")
+print("Hello my name is " +hello+ " I was born on " +answer)
+
+
+# Osei
+x=input("what is your name ? ")
+v=input("what would you like pizza Y or N ?")
+Y=input("three large slices for Osei")
+print("hello my name is Osei, x , v ")
+
+
+ 
+
+
+#This is Beneequa
+bname="Nikki" 
+print(bname)
+mom=2 # this is a interger value
+print(mom)      
+print("I love ny" ,mom)
+answer=input("what year were you born?")   
+print(" My name is "+bname+" and i was born in year "+answer)     
+
+#Luis Guardado 
+nombre="Luis" #this creates a variable for my name
+print(nombre)
+fecha=1993 #year of birth
+hospital="flushing" #hospital born in
+print (fecha , hospital)
+print("my name is " +nombre)
+con=input("What city were you born in?")
+print("I was born in "+hospital+ " where were you born?" +con)
+print("No way!")
+
+
+#This is Yanfei 
+#This create a variable for my name
+YF="This is Yanfei"
+print(YF)
+year = 1893
+country = "Czech"
+lucknum=9 # this is a integer variable
+favmusic="like classical music, such as Dvorak:Symphony No." # this is a string
+print(lucknum, favmusic)
+print("something about me" + favmusic ,lucknum," Composed in year", year)
+_answer_question=input("Where was Dvorak born?")
+print("Dvorak:Symphony No.9 composed in" + year)
+print("Hi!" + YF + favmusic,lucknum + "_answer_question+" " :)")
+      
+
+      
---
+++ Labs/Lbc17/lb17.js
@@ -0,0 +1,21 @@
+alert("Hello Cohort 5");
+document.write("In HTML, JavaScript programs are executed by the web browser.");
+document.write("<h3>JavaScript statements are composed of:<br>Values, Operators, Expressions, Keywords, and Comments.<br>This statement tells the browser to write 'Hello Dolly.' inside an HTML element with id='demo':</h3>");
+var x=5;
+var y=8;
+var z=x+y;
+console.log(x);
+console.log(y);
+console.log(z);
+var zuzz=1+25;
+console.log(zuzz);
+document.write(zuzz);
+function multFunct(p1,p2){
+ return p1*p2
+}
+ x= multFunct(y,zuzz);
+console.log(x);
+
+function toCelsius(fahrenheit) {
+  return (5/9) * (fahrenheit-32);
+}
